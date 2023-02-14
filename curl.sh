ed5abd21e42f925cb8dd86ee1f5f7d203e428b3a
ankam sandeep <ankamsandeep25@gmail.com>
2023-02-14 10:29:31 +0000

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,15 +1,17 @@
+# Current pipeline
+
 #!/bin/bash
+#https://gitlab.com/ankamsandeep/employee-portal
 
-# Define project and API access variables
+# Replace these placeholders with your actual values
 PROJECT_ID=42660645
-API_PRIVATE_TOKEN=glpat-9s3aPqFU1smmHxCzTWKh
+PRIVATE_TOKEN=glpat-9s3aPqFU1smmHxCzTWKh
 
-# Fetch the current running pipeline ID
-PIPELINE_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines?status=running" | jq '.[0].id')
+# Get the current running pipeline ID
+CURRENT_PIPELINE=$(curl --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines?status=running" | jq '.[0].id')
 
-# Fetch the current running job ID
-JOB_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID/jobs" | jq '.[0].id')
+# Get the latest running job ID for the pipeline
+CURRENT_JOB=$(curl --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines/$CURRENT_PIPELINE/jobs?status=running" | jq -r '.[].id' | tail -n 1)
 
-# Update the wiki with the latest artifacts link
-curl --request PUT --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" --data "content=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home" | jq '.content')\n\nArtifacts link: [Artifacts](https://gitlab.com/ankamsandeep/employee-portal/-/jobs/$JOB_ID/artifacts/browse)" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
-
+# Update the wiki with the link to the latest job's artifacts
+curl --request PUT --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" --data "content=New wiki content with link to current artifacts: [Artifacts for Job $CURRENT_JOB](https://gitlab.com/ankamsandeep/employee-portal/-/jobs/$CURRENT_JOB/artifacts/browse)" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
