971a820378e1cbaf0709f47b1c5712288c225e26
ankam sandeep <ankamsandeep25@gmail.com>
2023-02-14 10:20:15 +0000

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-# Replace with your GitLab project ID
-PROJECT_ID="42660645"
+# Define project and API access variables
+PROJECT_ID=42660645
+API_PRIVATE_TOKEN=glpat-9s3aPqFU1smmHxCzTWKh
 
-# Replace with your GitLab access token
-ACCESS_TOKEN="glpat-9s3aPqFU1smmHxCzTWKh"
+# Fetch the current running pipeline ID
+PIPELINE_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines?status=running" | jq '.[0].id')
 
-# Stages for which artifacts links should be updated
-STAGES=("build" "test")
+# Fetch the current running job ID
+JOB_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID/jobs" | jq '.[0].id')
 
-for STAGE in "${STAGES[@]}"; do
-  # Get the latest pipeline ID
-  PIPELINE_ID=$(curl --header "PRIVATE-TOKEN: $ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines?status=running" | jq '.[0].id')
+# Update the wiki with the latest artifacts link
+curl --request PUT --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" --data "content=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home" | jq '.content')\n\nArtifacts link: [Artifacts](https://gitlab.com/ankamsandeep/mynodeapp-cicd-project/-/jobs/$JOB_ID/artifacts/browse)" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
 
-  # Get the job ID for the stage
-  JOB_ID=$(curl --header "PRIVATE-TOKEN: $ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID/jobs" | jq ".[] | select(.stage == \"$STAGE\") | .id")
-
-  # Get the artifacts URL for the stage
-  ARTIFACTS_URL="https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs/$JOB_ID/artifacts"
-
-  # Get the current content of the wiki home page
-  WIKI_CONTENT=$(curl --header "PRIVATE-TOKEN: $ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home" | jq '.content')
-
-  # Update the wiki home page with the new artifacts link
-  curl --request PUT --header "PRIVATE-TOKEN: $ACCESS_TOKEN" --header "Content-Type: application/json" --data "{\"content\":\"$WIKI_CONTENT\n\nArtifacts for $STAGE stage: [Artifacts]($ARTIFACTS_URL) updated on $(date)\n\"}" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
-done
