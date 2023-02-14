b60a1f1a6b832ff7278e139d6c3b702af5933cca
ankam sandeep <ankamsandeep25@gmail.com>
2023-02-14 10:24:14 +0000

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -11,5 +11,5 @@ PIPELINE_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.
 JOB_ID=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID/jobs" | jq '.[0].id')
 
 # Update the wiki with the latest artifacts link
-curl --request PUT --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" --data "content=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home" | jq '.content')\n\nArtifacts link: [Artifacts](https://gitlab.com/ankamsandeep/mynodeapp-cicd-project/-/jobs/$JOB_ID/artifacts/browse)" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
+curl --request PUT --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" --data "content=$(curl --header "PRIVATE-TOKEN: $API_PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home" | jq '.content')\n\nArtifacts link: [Artifacts](https://gitlab.com/ankamsandeep/employee-portal/-/jobs/$JOB_ID/artifacts/browse)" "https://gitlab.com/api/v4/projects/$PROJECT_ID/wikis/home"
 
